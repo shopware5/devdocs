c67cbe5c62053616a3b257196fcabc6398697f70
Benjamin Cremer <bc@shopware.com>
2015-04-13 22:25:57 +0200

    Eating your own dog food
    Use bcremer/sculpin-commonmark-bundle to use the league/commonmark
    parser to generate html from markdown.

--- app/SculpinKernel.php
+++ app/SculpinKernel.php
@@ -1,12 +1,13 @@
 <?php
 use Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKernel;
-use Symbid\DevBlog\AuthorsBundle\SymbidAuthorsBundle;
+
 class SculpinKernel extends AbstractKernel
 {
     protected function getAdditionalSculpinBundles()
     {
         return [
-            'Shopware\Devdocs\SearchBundle\ShopwareSearchBundle'
+            'Shopware\Devdocs\SearchBundle\ShopwareSearchBundle',
+            'Bcremer\Sculpin\Bundle\CommonMarkBundle\SculpinCommonMarkBundle',
         ];
     }
 }

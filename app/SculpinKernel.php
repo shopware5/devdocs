123bbca8731a84c1689ac4bca323380e332dd94f
Benjamin Cremer <bc@shopware.com>
2015-04-14 14:59:50 +0200

    Revert "Eating your own dog food" Unfortunatly commonmark does not support tables (yet)
    This reverts commit c67cbe5c62053616a3b257196fcabc6398697f70.

--- app/SculpinKernel.php
+++ app/SculpinKernel.php
@@ -1,13 +1,12 @@
 <?php
 use Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKernel;
-
+use Symbid\DevBlog\AuthorsBundle\SymbidAuthorsBundle;
 class SculpinKernel extends AbstractKernel
 {
     protected function getAdditionalSculpinBundles()
     {
         return [
-            'Shopware\Devdocs\SearchBundle\ShopwareSearchBundle',
-            'Bcremer\Sculpin\Bundle\CommonMarkBundle\SculpinCommonMarkBundle',
+            'Shopware\Devdocs\SearchBundle\ShopwareSearchBundle'
         ];
     }
 }

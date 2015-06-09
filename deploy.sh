feefb101f487f354b1977b4d681739ac3659513d
Benjamin Cremer <bc@shopware.com>
2015-06-09 15:45:51 +0200

    devdocs.shopware.com -> developers.shopware.com

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o pipefail
 
 chmod 600 deploy_key
-echo -e "Host devdocs.shopware.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo -e "Host developers.shopware.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 eval `ssh-agent -s`
 ssh-add deploy_key
-rsync -avze ssh --rsync-path="sudo rsync" --exclude=".git" output_prod/ shopware@devdocs.shopware.com:/var/www/devdocs
+rsync -avze ssh --rsync-path="sudo rsync" --exclude=".git" output_prod/ shopware@developers.shopware.com:/var/www/developers.shopware.com

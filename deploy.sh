8569f3a23b194e72a84b80b7e3e21ea1797883f3
Benjamin Cremer <bc@shopware.com>
2015-07-02 16:49:42 +0200

    Move key decryption to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+openssl aes-256-cbc -K $encrypted_8d2e99642ba8_key -iv $encrypted_8d2e99642ba8_iv -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 echo -e "Host developers.shopware.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 eval `ssh-agent -s`

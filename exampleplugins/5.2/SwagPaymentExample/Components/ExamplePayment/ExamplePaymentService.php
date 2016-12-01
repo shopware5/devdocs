23ae246f392bbee96687218dd1f951da73dba3ea
Timo Altholtmann <T.Altholtmann@shopware.com>
2016-12-01 09:02:12 +0100

    SW-16272 Add article and example plugin for creating a payment plugin

---
+++ exampleplugins/5.2/SwagPaymentExample/Components/ExamplePayment/ExamplePaymentService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SwagPaymentExample\Components\ExamplePayment;
+
+class ExamplePaymentService
+{
+    /**
+     * @param $request \Enlight_Controller_Request_Request
+     * @return PaymentResponse
+     */
+    public function createPaymentResponse(\Enlight_Controller_Request_Request $request)
+    {
+        $response = new PaymentResponse();
+        $response->transactionId = $request->getParam('transactionId', null);
+        $response->status = $request->getParam('status', null);
+        $response->token = $request->getParam('token', null);
+        return $response;
+    }
+
+    /**
+     * @param PaymentResponse $response
+     * @param string $token
+     * @return bool
+     */
+    public function isValidToken(PaymentResponse $response, $token)
+    {
+        return hash_equals($token, $response->token);
+    }
+
+    /**
+     * @param float $amount
+     * @param int $customerId
+     * @return string
+     */
+    public function createPaymentToken($amount, $customerId)
+    {
+        return md5(implode('|', [$amount, $customerId]));
+    }
+}
---
+++ exampleplugins/5.2/SwagPaymentExample/SwagPaymentExample.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace SwagPaymentExample;
+
+use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\ActivateContext;
+use Shopware\Components\Plugin\Context\DeactivateContext;
+use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Components\Plugin\Context\UninstallContext;
+use Shopware\Models\Payment\Payment;
+
+class SwagPaymentExample extends Plugin
+{
+    /**
+     * @param InstallContext $context
+     */
+    public function install(InstallContext $context)
+    {
+        /** @var \Shopware\Components\Plugin\PaymentInstaller $installer */
+        $installer = $this->container->get('shopware.plugin_payment_installer');
+
+        $options = [
+            'name' => 'example_payment_invoice',
+            'description' => 'Example payment method invoice',
+            'action' => 'PaymentExample',
+            'active' => 0,
+            'position' => 0,
+            'additionalDescription' =>
+                '<!-- Logo start -->'
+                .'  <img src="http://your-image-url"/>'
+                .'<!-- Logo end -->'
+                .'<div id="payment_desc">'
+                .'  Pay save and secured by invoice with our example payment provider.'
+                .'</div>'
+        ];
+        $installer->createOrUpdate($context->getPlugin(), $options);
+
+        $options = [
+            'name' => 'example_payment_cc',
+            'description' => 'Example payment method credit card',
+            'action' => 'PaymentExample',
+            'active' => 0,
+            'position' => 0,
+            'additionalDescription' =>
+                '<!-- Logo start -->'
+                .'  <img src="http://your-image-url"/>'
+                .'<!-- Logo end -->'
+                .'<div id="payment_desc">'
+                .'  Pay save and secured by credit card with our example payment provider.'
+                .'</div>'
+        ];
+
+        $installer->createOrUpdate($context->getPlugin(), $options);
+    }
+
+    /**
+     * @param UninstallContext $context
+     */
+    public function uninstall(UninstallContext $context)
+    {
+        $this->setActiveFlag($context->getPlugin()->getPayments(), false);
+    }
+
+    /**
+     * @param DeactivateContext $context
+     */
+    public function deactivate(DeactivateContext $context)
+    {
+        $this->setActiveFlag($context->getPlugin()->getPayments(), false);
+    }
+
+    /**
+     * @param ActivateContext $context
+     */
+    public function activate(ActivateContext $context)
+    {
+        $this->setActiveFlag($context->getPlugin()->getPayments(), true);
+    }
+
+    /**
+     * @param Payment[] $payments
+     * @param $active bool
+     */
+    private function setActiveFlag($payments, $active)
+    {
+        $em = $this->container->get('models');
+
+        foreach ($payments as $payment) {
+            $payment->setActive($active);
+            $em->persist($payment);
+        }
+        $em->flush();
+    }
+}

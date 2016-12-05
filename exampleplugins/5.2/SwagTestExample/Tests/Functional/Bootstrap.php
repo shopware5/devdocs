2d1d8ba9ffc3d94ebc54ea268526084401afaa9a
Thomas Eiling <teiling88@gmail.com>
2016-12-05 10:35:51 +0100

    misc - unify of Tests directory usage (#440)

--- exampleplugins/5.2/SwagTestExample/Tests/Functional/Bootstrap.php
+++ exampleplugins/5.2/SwagTestExample/Tests/Functional/Bootstrap.php
@@ -4,5 +4,4 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 
-require __DIR__ . '/../../../../../tests/Functional/bootstrap.php';
-
+require __DIR__ . '/../../../../../Tests/Functional/bootstrap.php';

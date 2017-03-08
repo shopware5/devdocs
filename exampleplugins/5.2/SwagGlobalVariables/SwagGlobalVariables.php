1c60a13654b70c7df9170b94827dd6493b9dc040
Timo Altholtmann <T.Altholtmann@shopware.com>
2017-03-08 10:29:53 +0100

    SW-17454 - Add using global variables in templates article and plugin

---
+++ exampleplugins/5.2/SwagGlobalVariables/SwagGlobalVariables.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SwagGlobalVariables;
+
+use Shopware\Components\Plugin;
+
+class SwagGlobalVariables extends Plugin
+{
+    /**
+     * @inheritdoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Enlight_Controller_Action_PostDispatchSecure_Frontend' => 'onPostDispatch',
+            'Enlight_Controller_Action_PostDispatchSecure_Widgets' => 'onPostDispatch'
+        ];
+    }
+
+    /**
+     * @param \Enlight_Controller_ActionEventArgs $args
+     */
+    public function onPostDispatch(\Enlight_Controller_ActionEventArgs $args)
+    {
+        $args->getSubject()->View()->assign('sUserloggedIn', Shopware()->Modules()->Admin()->sCheckUser());
+    }
+}

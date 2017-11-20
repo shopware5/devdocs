6bda22fa8235e535a03e15b7d1eca2f34798f3ff
Philip Gatzka <p.gatzka@shopware.com>
2017-11-20 13:44:30 +0100

    Remove unnecessary build method override

--- exampleplugins/5.2/SwagCustomRiskRule/SwagCustomRiskRule.php
+++ exampleplugins/5.2/SwagCustomRiskRule/SwagCustomRiskRule.php
@@ -7,9 +7,5 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class SwagCustomRiskRule extends Plugin
 {
-    public function build(ContainerBuilder $container)
-    {
-        $container->setParameter('swag_custom_risk_rule.plugin_dir', $this->getPath());
-        parent::build($container);
-    }
 }
+
